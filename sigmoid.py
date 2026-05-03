d1ebb738945daaf5be78d2bfa7b8897cbc21a3bb
sarahxiangg <sarahx1311@gmail.com>
2026-05-03 17:57:56 +1000

    sigmoid

---
+++ sigmoid.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+class SigmoidLayer():
+    def __init__(self, n: int):
+        self.n = n
+
+    def forward(self, x: np.ndarray) -> np.ndarray:
+        self.x = x  
+        return 1 / (1 + np.exp(-x))
